9ad253aca362d63f16483617cdaee20524d71d68
tasha maina <tashabanks271@gmail.com>
2025-09-25 13:04:40 +0300

    Completed anagram lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        """
+        Return all words from `candidates` that are anagrams of self.word.
+        
+        """
+        #Normalize the word: sorted letters
+        target = sorted(self.word)
+
+        matches = []
+        for candidate in candidates:
+            if sorted(candidate) == target:
+                matches.append(candidate)
+        return matches 
+        
